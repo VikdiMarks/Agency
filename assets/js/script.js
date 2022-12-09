c7f23de50a7e4c3c39c88191a2085c34cea25bbe
VikdiMarks <sarankov.dima@gmail.com>
2022-12-10 00:03:16 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+/*=========================================================================
+                    Preloader
+=========================================================================*/
+$(document).ready(function () {
+    setTimeout(function(){
+        document.getElementById('preloader').style.display = "none";
+        title.innerHTML = "Home";
+    },3000);
+
+    // CHANGE STEPS CLASS WHILE CHECKING RESPONSIVE
+    $(window).resize(function() {
+        if(window.innerWidth < 575){
+            $("#step2").removeClass("apr-count-r");
+            $("#step4").removeClass("apr-count-r");
+            $("#step2").addClass("apr-count-l");
+            $("#step4").addClass("apr-count-l");
+        }
+        else{
+            $("#step2").removeClass("apr-count-l");
+            $("#step4").removeClass("apr-count-l");
+            $("#step2").addClass("apr-count-r");
+            $("#step4").addClass("apr-count-r");
+        }
+    });
+});
+
+/*=========================================================================
+                    Analys Form
+=========================================================================*/
+const dropForm = document.querySelector('#dropForm');
+const icForm = document.querySelector('#ic-form');
+document.querySelector('#frmBtn').addEventListener('click',function(){
+    if (dropForm.style.display !== "none") {
+        dropForm.style.display = "none";
+        dropForm.style.height = "0%";
+    } else {
+        dropForm.style.display = "block";
+        dropForm.style.height = "100%";
+    }
+    $(this).toggleClass("rotate");
+});
+document.querySelector('#close-form').addEventListener('click',function(){
+    if (dropForm.style.display !== "none") {
+        dropForm.style.display = "none";
+    } else {
+        dropForm.style.display = "block";
+    }
+    $('#frmBtn').toggleClass("rotate"); 
+});
+
+/*=========================================================================
+                    FIXED MENUBAR
+=========================================================================*/
+function scrollBody() {
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            var scrollheight = $(window).scrollTop();
+            if (scrollheight > 80) {
+                $("#menu").css("background", "#fff");
+                $("#menu").addClass("fixed-menu");
+                $("#menu").addClass("shadow-head");
+            } else {
+                $("#menu").removeClass("fixed-menu");
+                $("#menu").removeClass("shadow-head");
+            }
+        });
+    });
+}
+
+// SET STEPS CLASS ONLOAD
+function screenSize(){
+    if(window.innerWidth < 575){
+        if(document.querySelector('#step2').classList.contains('apr-count-r')){
+            $("#step2").removeClass("apr-count-r");
+            $("#step4").removeClass("apr-count-r");
+        }
+        $("#step2").addClass("apr-count-l");
+        $("#step4").addClass("apr-count-l");
+    }
+    else{
+        if(document.querySelector('#step2').classList.contains('apr-count-r')){
+            $("#step2").removeClass("apr-count-l");
+            $("#step4").removeClass("apr-count-l");
+        }
+        $("#step2").addClass("apr-count-r");
+        $("#step4").addClass("apr-count-r");
+    }
+}
+
+
+/*=========================================================================
+                    Approach Steps Mouse Follow Effect
+=========================================================================*/
+VanillaTilt.init(document.querySelector(".apr-box"), {
+    max: 25,
+    speed: 400
+});
+VanillaTilt.init(document.querySelectorAll(".apr-box"));
